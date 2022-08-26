9ed75692dbc3861ba024938b50d51678e6f3d381
SpikeSer <spikesoriginal@gmail.com>
2022-08-26 19:39:28 +0300

    Lecture48:Constructors,prototype,this

---
+++ site/Lecture48/js/script.js
@@ -0,0 +1,13 @@
+// Function constructors
+
+function Circle(radius) {
+	this.radius = radius
+}
+Circle.prototype.getArea = function () {
+	Math.PI * Math.pow(this.radius, 2)
+}
+var myCircle = new Circle(10) //new Object();
+console.log(myCircle)
+
+var myOtherCircle = new Circle(20)
+console.log(myOtherCircle)
